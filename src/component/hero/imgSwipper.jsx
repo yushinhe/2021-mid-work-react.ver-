6b6c02ab5cf94bfabda429862b2703e4560a557a
yushinhe <robin124708476@gmail.com>
2021-04-29 14:38:35 +0800

    回去調東西

---
+++ src/component/hero/imgSwipper.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { EffectCoverflow } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/effect-coverflow/effect-coverflow.scss';
+import img1 from '../../img/product-img/book2.png'
+import img2 from '../../img/product-img/book3.png'
+import img3 from '../../img/product-img/book4.png'
+import img4 from '../../img/product-img/book5.png'
+import img5 from '../../img/product-img/book6.png'
+import img6 from '../../img/product-img/book7.png'
+
+SwiperCore.use([EffectCoverflow]);
+let imgArr = [img1, img2, img3, img4, img5, img6]
+function Swipper() {
+  return (
+    <Swiper
+      grabCursor={true}
+      centeredSlides={true}
+      slidesPerView={'auto'}
+      coverflowEffect={
+        { rotate: 0 },
+        { stretch: 0 },
+        { depth: 200 },
+        { modifier: 1 },
+        { slideShadows: true }
+      }
+      autoplay={
+        { delay: 1000 },
+        { speed: 400 }
+      }
+      loop={true}>
+      {imgArr.map((i) => {
+        console.log(i);
+        return <SwiperSlide key={`slide-${i}`}><img src={i} /></SwiperSlide>;
+      })}
+    </Swiper>
+  );
+};
+
+export default Swipper;
--- src/component/hero/index.jsx
+++ src/component/hero/index.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import './index.css'
 // import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/swiper-bundle.css';
+import ImgSwiper from './imgSwipper';
 export default class Hero extends Component {
     constructor(props) {
         super(props)
@@ -14,29 +12,24 @@ export default class Hero extends Component {
                     detail: '太公望書林提供了很多能豐富精神的書籍，我們希望我們的讀者讀了我們的書之後，除了能擁有物質上的黃金屋，也能在書中探索到自己心中的黃金屋'
                 }
             ],
-            swiperImg:[
-
-            ]
-        }
+           }
     }
-
     render() {
-
-        result(
+        return(
             <>
                 <section className="hero">
                     <div className="bg"></div>
                     <div className="container grid">
                         <div className="info">
-                            <h2>{this.state.info.title}</h2>
-                            <p>{this.state.info.detail}</p>
+                            <h2>{this.state.info[0].title}</h2>
+                            <p>{this.state.info[0].detail}</p>
                             <a href="./product-list.html" className="btn btn-black"> 立刻選書 &nbsp; <i
                                 className="fas fa-chevron-right"></i></a>
 
                         </div>
                         <div className="swiper-container">
                             <div className="swiper-wrapper">
-                            <div class="swiper-slide"><img src="./product-img/${swiperImg[i]}"/></div>
+                                <ImgSwiper />
                             </div>
                         </div>
                         <div className="d-flex icon">
