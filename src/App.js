393ddc5328d215b8891bf4d2990e5b9e70b0c9d6
yushinhe <robin124708476@gmail.com>
2021-05-02 13:41:33 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
 import React from "react";
+import Home from './page/Home'
 import {
   BrowserRouter as Router,
+  Switch,
   Route,
   Link,
-  Switch,
-  Redirect,
-  useLocation
+  useRouteMatch,
+  useParams
 } from "react-router-dom";
-import Home from './page/Home'
-// You can use the last <Route> in a <Switch> as a kind of
-// "fallback" route, to catch 404 errors.
-//
-// There are a few useful things to note about this example:
-//
-// - A <Switch> renders the first child <Route> that matches
-// - A <Redirect> may be used to redirect old URLs to new ones
-// - A <Route path="*> always matches
-
-export default function NoMatchExample() {
-  return ( 
+export default function App() {
+  return (
+    <Router>
       <div>
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/old-match">
-            <Redirect to="/will-match" />
-          </Route>
-          <Route path="/will-match">
-            <WillMatch />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
+          <Route exact path="/about" component={() => 'About'}/>
+      
+          <Route exact path="/topics" component={() => 'Topics'}/>
+        
+          <Route exact path="/" component={Home}/>       
+          <Route component={() => 'Error'}/>
         </Switch>
       </div>
-  );
-}
-
-
-function WillMatch() {
-  return <h3>Matched!</h3>;
-}
-
-function NoMatch() {
-  let location = useLocation();
-
-  return (
-    <div>
-      <h3>
-        No match for <code>{location.pathname}</code>
-      </h3>
-    </div>
+    </Router>
   );
 }
