068e2cd3802793c55d25d74cfd1233ef2a1eca56
yushinhe <robin124708476@gmail.com>
2021-05-08 17:18:08 +0800

    小調整

--- src/component/ArticleDetail/index.jsx
+++ src/component/ArticleDetail/index.jsx
@@ -8,7 +8,7 @@ export default class index extends Component {
         super(props);
         this.state = {
             data: props.Data,
-            recommandData: Data.find(item => item.id == Math.ceil(Math.random() * 9)),
+            recommandData: Data.find(item => item.id === '2'),
         };
     }
     render() {
@@ -19,8 +19,8 @@ export default class index extends Component {
                         <img src={this.state.data.img.main} alt="{this.state.data.title}" />
                     </section>
                     <main>
-                        <div class="container">
-                            <ul class="menu">
+                        <div className="container">
+                            <ul className="menu">
                                 <li>
                                     <Link to='/articleList'>所有文章</Link>
                                 </li>
@@ -37,14 +37,14 @@ export default class index extends Component {
                                     {this.state.data.title}
                                 </li>
                             </ul>
-                            <div class="title">
+                            <div className="title">
                                 <h2>{this.state.data.title}</h2>
                                 <h3>{this.state.data.subtitle}</h3>
                             </div>
                         </div>
-                        <div class="container">
-                            <div class="content-container">
-                                <ul class="write-info">
+                        <div className="container">
+                            <div className="content-container">
+                                <ul className="write-info">
                                     <li>
                                         <FaUser className="write-icon" />羅子豪
                                     </li>
@@ -55,7 +55,7 @@ export default class index extends Component {
                                         <FaTag className="write-icon" />{this.state.data.location}
                                     </li>
                                 </ul>
-                                <article>
+                                <article className="content-article">
                                     <p>{this.state.data.intro}
                                     </p>
                                     <p>電力實際上訊息創新您是，也沒考慮變化，而來設立提示重複內容簡介衛生上面眼淚以前整合大約，過去加盟鐵路意味着，詳細學者我來旁邊備案教師直到均為，你還配套建築門派一臉不錯，臺中引進經濟發展意外但在，公園。
@@ -65,16 +65,18 @@ export default class index extends Component {
                                 </article>
                             </div>
                         </div>
-                        <figure class="big-pic container">
+                        <figure className="big-pic container">
                             <img src={this.state.data.img.introImg} alt={this.state.data.img.imgIntro} />
                             <figcaption>{this.state.data.img.imgIntro} </figcaption>
                         </figure>
-                        <div class="content-container container">
-                            <div class="famous-saying">
+                        <div className="content-container container">
+                            <div className="famous-saying">
                                 <h4>如果這世界上真的有神，那台灣巨王棒棒雞一定是神送給這世界最美好的禮物</h4>
-                                <h4> <i class="fas fa-user-alt"></i>知名冒險家-李恆昇</h4>
+                                <h4>                                         <FaUser className="write-icon" />
+                                    知名冒險家-李恆昇
+                                </h4>
                             </div>
-                            <article>
+                            <article className="content-article">
                                 <p>
                                     天然流程，隱藏指南多麼下去是否電力作品每頁分配啟動一段時間勞動，孤獨不少高度會不會知名戀愛網通是這樣國際試驗，看到貼圖很有不知青年存儲世紀控制常見改造不同近年來驚訝，眼淚黑暗，詳細內容情人那天一人之外會議信用聯合違反據說貼圖請您，上次心理條款外國，模糊角度臨時優惠預覽努力改進萬元，查找詳細創新發揮天堂打造相互將會先進教育中心裝修話題，您可以電子瀏覽次數，斗內經濟發展一家，我和手指結合網絡回事教師。
                                 </p>
@@ -87,43 +89,43 @@ export default class index extends Component {
                                 <p>
                                     相互這時像是春節解決方案內容簡介醫療支持尷尬，首頁企業漂亮腦袋考察通常見過危機地說臉色而且，配件網際快車手裡有時，我說連續，或者作家一番展開現代深入當天培訓以往分為知名如何曾經，實驗哪些準備不可以學校身邊，確保帝國你就說話之前使用深深輸入贏得感到，興奮化工驚。
                                 </p>
-                                <ul class="social">
+                                <ul className="social">
                                     <li>
-                                        <FaHeart />&nbsp;&nbsp; 527
+                                        <FaHeart className="icon"/>&nbsp;&nbsp; 527
                                     </li>
                                     <li>
                                         <FaShareAlt />&nbsp; &nbsp;分享
                                     </li>
                                 </ul>
-                                <form action="" class="message">
+                                <form action="" className="message">
                                     <textarea name="" id="" placeholder="留下您的心得..." required></textarea>
-                                    <button type="submit" class="btn btn-gray">送出</button>
+                                    <button type="submit" className="btn btn-gray">送出</button>
                                 </form>
                             </article>
-                            <div class="also-recommand">
-                                <div class="recommand">
+                            <div className="also-recommand">
+                                <div className="recommand">
                                     <img src={this.state.recommandData.img.main} alt={this.state.recommandData.title} />
-                                    <span>相關推薦</span>
+                                    <span>本月推薦</span>
                                     {this.state.recommandData.title}
                                 </div>
                             </div>
                         </div>
-                        <section class="change-page">
-                            <div class="container item-group">
-                                <a href="#" class="item">
-                                    <div class="icon">
+                        <section className="change-page">
+                            <div className="container item-group">
+                                <a href="#" className="item">
+                                    <div className="icon">
                                         <FaArrowLeft />
                                     </div>
-                                    <div class="toPage">
+                                    <div className="toPage">
                                         <span>上一主題</span>
                                         <h3>嚴娜亞羅十六僧</h3>
                                     </div>
                                 </a>
-                                <a href="#" class="item">
-                                    <div class="icon">
+                                <a href="#" className="item">
+                                    <div className="icon">
                                         <FaArrowRight />
                                     </div>
-                                    <div class="toPage">
+                                    <div className="toPage">
                                         <span>下一主題</span>
                                         <h3>埃及霓光螢火蟲</h3>
                                     </div>
